e13b0a0bfb3da7119ce3536b5fe0c033c9835343
Reginaldo Morikawa <morikawa77@Morcegao.local>
2019-02-16 05:04:05 -0200

    Adicionado ícone

--- forca/Form1.Designer.cs
+++ forca/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Forca));
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -229,6 +230,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Forca";
             this.Text = "Jogo da Forca";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
