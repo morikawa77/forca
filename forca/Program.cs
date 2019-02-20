b6b68ea1111f96614534167e8fcbd0acc43f654d
Reginaldo Morikawa <morikawa77@Morcegao.local>
2019-02-20 01:38:31 -0300

    Adicionado Splash Screen

--- forca/Program.cs
+++ forca/Program.cs
@@ -16,7 +16,7 @@ namespace forca
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Forca());
+            Application.Run(new SplashScreen());
         }
     }
 }
